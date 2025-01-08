69a093fbbb6527f6abc362d914493a31e3d83e9c
lisa <lisaauta018@gmail.com>
2025-01-08 17:14:11 +0300

    added icons

--- src/component 2/comp2.js
+++ src/component 2/comp2.js
@@ -6,6 +6,12 @@ function Connects() {
             <div className="text2">
               <h3>Not your average book club</h3>
               <p>Connect with a community that speaks your language - from <b>Python</b> to <b>Typescript</b> and everything in between. Our discussions blend technical depth with practical applications.</p>
+              <br></br>
+              <div className="icons">
+              <i class="fa-brands fa-github" style={{color: '#6F06A7'}}></i>
+              <i class="fa-brands fa-react" style={{color: '#6F06A7'}}></i>
+              <i class="fa-brands fa-python" style={{color: '#6F06A7'}}></i>
+              </div>
             </div>
             <div>
               <img src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="reading together"></img>
