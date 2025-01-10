60bad6896ffc8ecc49204acc98531c882373178b
lisa <lisaauta018@gmail.com>
2025-01-10 23:21:33 +0300

    component 5 looking good

--- src/component 5/maincomp.js
+++ src/component 5/maincomp.js
@@ -4,6 +4,12 @@ function ComponentFive(){
     return(
         <div className="five">
             <div className="stars"> {String.fromCodePoint('0x2b50')} {String.fromCodePoint('0x2b50')} {String.fromCodePoint('0x2b50')} {String.fromCodePoint('0x2b50')} {String.fromCodePoint('0x2b50')} </div>
+            <div className="remarks">
+                <p>"This book club transformed my technical reading from a solitary activity into an enriching community experience. The discussions are gold!"</p>
+            </div>
+            <div className="author">
+                <p>Sarah Chen, Software Architect</p>
+            </div>
         </div>
     )
 }
