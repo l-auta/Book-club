ec9edeb8c3c9905b35d660f49b4e686898a5f239
lisa <lisaauta018@gmail.com>
2025-01-12 14:24:43 +0300

    component one mobile complete

--- src/component 1/comp2.js
+++ src/component 1/comp2.js
@@ -9,9 +9,9 @@ function Description() {
               <br></br>
               <button className="membership">REVIEW MEMBERSHIP OPTIONS  {String.fromCodePoint('0x1f447')}</button>
               </div>
-          <div className="pic">
-            <img src="https://images.unsplash.com/photo-1630715216350-1a8f46f021bf?q=80&w=1865&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="people" />
-          </div>
+            <div className="pic">
+              <img src="https://images.unsplash.com/photo-1630715216350-1a8f46f021bf?q=80&w=1865&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="people" />
+            </div>
         </div>
     )
 }
